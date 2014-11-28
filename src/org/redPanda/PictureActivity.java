519c1ea290d4489e076183771804116b71882aed
Pizza007 <malte.flohr@gmail.com>
2014-11-28 13:12:49 +0100

    Cleaned up code for the in App Image handeling + added tap on image to close

--- src/org/redPanda/PictureActivity.java
+++ src/org/redPanda/PictureActivity.java
@@ -6,14 +6,14 @@
 package org.redPanda;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,102 +27,39 @@ import pl.droidsonroids.gif.GifImageView;
  */
 public class PictureActivity extends Activity {
 
-    GifImageView mGifImageView;
+    
     PhotoViewAttacher mAttacher;
+    Context con;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        con = this;
         getWindow().setBackgroundDrawable(new ColorDrawable(0x80000000));
         Intent in = getIntent();
-//
-        String path = in.getExtras().getString("path");
-//        String[] asd = path.split("/");
-//        path = asd[asd.length - 1];
-//        String mainpath = "file:///" + Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pictures/redPanda/";
-//
-//        int pic_height = in.getExtras().getInt("Pic_Height");
-//        int pic_width = in.getExtras().getInt("Pic_Width");
-////        RelativeLayout rl = (RelativeLayout) getLayoutInflater().inflate(R.layout.picture_activity, null);
-////        rl.setBackgroundColor(0x80000000);
-//
-////        WebView wv = (WebView) rl.findViewById(R.id.PictureView);
-//        WebView wv = new WebView(this);
-//        wv.getSettings().setLoadWithOverviewMode(true);
-//        wv.getSettings().setUseWideViewPort(true);
-//        //   wv.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
-////        wv.setPadding(0, 0, 0, 0);
-////        wv.setInitialScale(getScale(pic_width));
-//        wv.setBackgroundColor(0x80000000);
-//        // wv.setInitialScale(getScale(pic_width));
-//        String data;
-//        double scale = getScale(pic_width, pic_height);
-//        String temp = scale+"\n"+pic_width+" "+pic_height;
-//        Double ph = new Double(pic_width) * scale;
-//        pic_width = ph.intValue();
-//        ph = new Double(pic_height) * scale;
-//        pic_height = ph.intValue();
-//        temp +="\n"+pic_width+" "+pic_height;
-//        Toast.makeText(this,temp, Toast.LENGTH_LONG).show();
-////        data = "<table width=\"100%\" height=\"100%\" align=\"center\" valign=\"center\">\n"
-////                + "     <tr><td>\n"
-////                + "    \n"
-////                + "    <div id=\"wrapper\" style=\"text-align: center\">    \n"
-////                + "    <div id=\"yourdiv\" style=\"display: inline-block;\"><img src=\""
-////                + path
-////                + "\" width=\""
-////                + 50
-////                + "\" height=\""
-////                + 50
-////                + "\" alt=\"foo\" /></div>\n"
-////                + "</div>\n"
-////                + "     \n"
-////                + "                      </td></tr>\n"
-////                + "</table>";
-//
-//        data = "<html>\n"
-//                + "<body>\n"
-//                + "<table width=\"100%\" height=\"100%\">\n"
-//                + "<tr><td>\n"
-//                + "    \n"
-//                + "<div style=\"text-align: center\">    \n"
-//                + "    <div style=\"display: inline-block;\"><img src=\""
-//                +path
-//                + "\" width=\""
-//                +pic_width
-//                + "\" height=\""
-//                +pic_height
-//                + "\"> </div>\n"
-//                + "</div>\n"
-//                + "     \n"
-//                + "</td></tr>\n"
-//                + "</table>\n"
-//                + "</body>\n"
-//                + "</html>";
-//
-//        wv.loadDataWithBaseURL(mainpath, data, "text/html", "utf-8", "");
-//        // wv.loadDataWithBaseURL(mainpath, data, "text/html", "utf-8", null);
-//
-//        wv.getSettings().setBuiltInZoomControls(true);
-//        wv.getSettings().setDisplayZoomControls(false);
-//        wv.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-//        setContentView(wv);
-
-        super.onCreate(savedInstanceState);
-        mGifImageView = new GifImageView(this);
-        ImageView mImageView = null;
-        // final Bitmap bitmap = FlActivity.getBitmapFromMemCache(path);
-        boolean isgif = true;
-        if (true){//(path.endsWith(".jpg")) { // HACK because all pictures are saved as .jpg
+        String path = in.getExtras().getString("path");       
+        GifImageView mGifImageView;
+        ImageView mImageView = null;      
+        //Check if the Image ends with ".gif"
+        boolean isgif = false;
+        
+        
+        if (true) {//(path.endsWith(".gif")) { // HACK because all pictures are saved as .jpg
+            
+            
+            
+            //Try to load gif in GifImageView
+            isgif = true;
             try {
+                mGifImageView = new GifImageView(this);
                 mGifImageView.setImageDrawable(new GifDrawable(path));
                 mImageView = mGifImageView;
             } catch (IOException ex) {
                 Logger.getLogger(PictureActivity.class.getName()).log(Level.SEVERE, null, ex);
-                isgif = false;
+                isgif = false;               
             }
-        } else {
-            isgif = false;
         }
+        //If the file is not a gif or loading the gif fails display it as bitmap
         if (!isgif) {
             mImageView = new ImageView(this);
             BitmapFactory.Options options = new BitmapFactory.Options();
@@ -131,15 +68,14 @@ public class PictureActivity extends Activity {
             mImageView.setImageBitmap(bitmap);
         }
         mAttacher = new PhotoViewAttacher(mImageView);
+        mAttacher.setOnViewTapListener(new PhotoViewAttacher.OnViewTapListener() {
+
+            public void onViewTap(View view, float x, float y) {
+                // Close the Activity when taped on the ImageView
+                finish();
+            }
+        });
         setContentView(mImageView);
 
-    }
-
-    private double getScale(int Pic_Width, int Pic_Height) {
-
-        int width = ChatAdapter.imageMaxSize;
-        int height = Resources.getSystem().getDisplayMetrics().heightPixels;
-        Double val = Math.min(new Double(width) / new Double(Pic_Width), new Double(height) / new Double(Pic_Height));
-        return val;
     }
 }
